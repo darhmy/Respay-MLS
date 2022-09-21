8aa79d3bc3751fc224295e93c28f4a66f71b3ca7
PetrOgundipe <ogundipepeterdare@gmail.com>
2022-09-21 09:33:19 +0100

    Added Project Dependencies

---
+++ RespayMLS.Data/RespayDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RespayMLS.Data
+{
+    public class RespayDbContext : DbContext
+    {
+        public RespayDbContext(DbContextOptions<RespayDbContext> options): base(options)
+        {
+
+        }
+    }
+}
